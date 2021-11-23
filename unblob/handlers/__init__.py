16513f119ec702144ebe167fc2784bca4cc74e51
Quentin Kaiser <quentin@iot-inspector.com>
2021-11-23 10:38:56 +0100

    initial support for ISO9660

--- unblob/handlers/__init__.py
+++ unblob/handlers/__init__.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from ..models import Handler
 from .archive import cab, cpio, tar, zip
+from .filesystem import iso9660
 
 
 def _make_handler_map(*handlers: Handler) -> Dict[str, Handler]:
@@ -17,5 +18,6 @@ _ALL_MODULES_BY_PRIORITY: List[Dict[str, Handler]] = [
         cpio.PortableASCIIWithCRCHandler,
         cpio.PortableOldASCIIHandler,
         cpio.BinaryHandler,
+        iso9660,
     ),
 ]
