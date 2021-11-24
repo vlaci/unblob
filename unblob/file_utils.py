7c0bd4156aceea6fef227d179c8edb1256e6d03f
takeshi <takeshi@iot-inspector.com>
2021-11-24 14:27:29 +0100

    Add ZIP handler

--- unblob/file_utils.py
+++ unblob/file_utils.py
@@ -1,5 +1,6 @@
 import io
 import math
+import os
 
 
 def snull(content: bytes):
@@ -10,6 +11,24 @@ def snull(content: bytes):
 def round_up(size: int, alignment: int):
     """Round up size to the alignment boundary."""
     return alignment * math.ceil(size / alignment)
+
+
+def find_first(file: io.BufferedIOBase, pattern: bytes) -> int:
+    chunk_size = 0x1000
+    compensation = len(pattern) - 1
+    bytes_searched = 0
+    while True:
+        # Prepend the padding from the last chunk, to make sure that we find the pattern, even if
+        # it straddles the chunk boundary.
+        data = file.read(chunk_size)
+        if data == b"":
+            # We've reached the end of the stream.
+            return -1
+        marker = data.find(pattern)
+        if marker != -1:
+            return marker + bytes_searched
+        file.seek(-compensation, os.SEEK_CUR)
+        bytes_searched += chunk_size - compensation
 
 
 class LimitedStartReader(io.BufferedIOBase):
