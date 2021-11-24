81d375ed2115d3bba1d21450cff763f0d890a70a
János Kukovecz <janos@iot-inspector.com>
2021-11-24 13:56:04 +0100

    Add test for missing integration handlers
    This test relies for the directory structure in thest "tests/integeation" directory.
    The structure should be like:
    ...tests/integration/...<no-matter-how-deep-the-structure-is>.../<handler-name>/<test-name>/
    Where the:
        <test-name> is the test case directory, which contains the __input__
    and __output__ folders.
        <handler-name> is the actual name of the hander, defined as the NAME property of every Handler.

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -12,6 +12,7 @@ from pathlib import Path
 
 import pytest
 
+from unblob.handlers import _ALL_MODULES_BY_PRIORITY
 from unblob.processing import DEFAULT_DEPTH, process_file
 
 TEST_DATA_PATH = Path(__file__).parent / "integration"
@@ -40,3 +41,20 @@ def test_all_handlers(input_dir: Path, output_dir: Path, tmp_path: Path):
         subprocess.run(diff_command, capture_output=True, check=True, text=True)
     except subprocess.CalledProcessError as exc:
         pytest.fail(exc.stdout)
+
+
+def test_missing_handlers_integrations_tests():
+    tested_handler_names = set(d.parent.name for d in TEST_CASE_DIRS)
+    all_handler_names = set(
+        name for handler in _ALL_MODULES_BY_PRIORITY for name in handler
+    )
+
+    no_test_for_handers = all_handler_names - tested_handler_names
+    assert (
+        no_test_for_handers == set()
+    ), f"Tests not found for handler(s): {no_test_for_handers}"
+
+    no_handler_for_tests = tested_handler_names - all_handler_names
+    assert (
+        no_handler_for_tests == set()
+    ), f"No handler found for these test(s) (???): {no_handler_for_tests}"
